ec844c058d0ba944044df59b207de07ac4bda464
VirgoAsumita <2256806298@qq.com>
2022-09-07 10:49:28 +0800

    第三提交文件

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 export const person = new Object();
 person.name = "张三";
-console.log(person.name); 
+console.log(person.name);
+const div = document.querySelector('div');
+div.onclick = () => {
+    console.log(div.offsetHeight);
+}
